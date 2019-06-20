a3a9cc44692490be70261230c179aa54600bc583
alaraiabdiallah <alaraiabdiallah@gmail.com>
2019-06-20 15:20:58 +0700

    add try catch

--- providers/Webhook/index.js
+++ providers/Webhook/index.js
@@ -8,16 +8,20 @@ class Webhook {
 
 
     handleRequest({ request, response }){
-        const agent = new WebhookClient({ request, response });
-  
-        console.log('Dialogflow Request headers: ' + JSON.stringify(request.headers));
-        console.log('Dialogflow Request body: ' + JSON.stringify(request.body));
-        
-        let intentMap = new Map();
-        for(let i = 0; i < intents.length;i++)
-            intentMap.set(intents[i].name, require(`../../app/Webhook/Intents/${intents[i].handler}`));
-        
-        agent.handleRequest(intentMap);
+        try {
+            const agent = new WebhookClient({ request, response });
+      
+            console.log('Dialogflow Request headers: ' + JSON.stringify(request.headers));
+            console.log('Dialogflow Request body: ' + JSON.stringify(request.body));
+            
+            let intentMap = new Map();
+            for(let i = 0; i < intents.length;i++)
+                intentMap.set(intents[i].name, require(`../../app/Webhook/Intents/${intents[i].handler}`));
+            
+            agent.handleRequest(intentMap);
+        } catch (error) {
+            console.error(error);
+        }
     }
 
 
